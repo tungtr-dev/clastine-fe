93e81a3e1f9f2116b21094db828ea790ef26c1e2
Tung Tran <tungtr.dev@gmail.com>
2026-02-01 13:37:57 +0700

    ui(common): icon

---
+++ src/components/common/data/index.ts
@@ -0,0 +1,2 @@
+export { IconView } from "./icon/icon.view.tsx";
+export type { IIconViewProperties } from "./icon/icon.view.interface.ts";
---
+++ src/constants/svg-source.enum.ts
@@ -0,0 +1,3 @@
+export enum SVGSource {
+	Dashboard = "dashboard"
+};
