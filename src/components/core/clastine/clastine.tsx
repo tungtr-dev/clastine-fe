eb45dad4ba2fbb876c2606536e783d8b08fe504f
Tung Tran <tungtr.dev@gmail.com>
2026-02-02 22:37:02 +0700

    core(theme): base theme

--- src/components/core/clastine/clastine.tsx
+++ src/components/core/clastine/clastine.tsx
@@ -1,17 +1,24 @@
 import { MenuItemView } from "@common/navigation/index.ts";
 import { SVGSource } from "@constants/svg-source.enum.ts";
+import { useContext } from "react";
+import { ThemeContext } from "@contexts/theme.context.ts";
 import "./clastine.scss";
+import "./clastine.theme.scss";
 
 export const Clastine = () => {
-	return (<>
-		{[
-			"Dashboard",
-			"Classrooms",
-			"Calendar"
-		].map(label => (
-			<MenuItemView icon={SVGSource.Dashboard}>
-				{label}
-			</MenuItemView>
-		))}
-	</>);
+	const theme = useContext(ThemeContext);
+
+	return (
+		<div className="clastine" current-theme={theme}>
+			{[
+				"Dashboard",
+				"Classrooms",
+				"Calendar"
+			].map(label => (
+				<MenuItemView icon={SVGSource.Dashboard}>
+					{label}
+				</MenuItemView>
+			))}
+		</div>
+	);
 };
