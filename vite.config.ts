fcc861e39e046502ff7125c8c9a82278656ab4b8
Tung Tran <tungtr.dev@gmail.com>
2026-02-07 10:58:47 +0700

    ui(style): global styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
+import { resolve } from "path";
 
 export default defineConfig({
 	server: {
 		port: 5050
+	},
+	resolve: {
+		alias: {
+			"@styles": resolve(__dirname, "src/styles")
+		}
 	},
 	plugins: [
 		tsConfigPaths(),
