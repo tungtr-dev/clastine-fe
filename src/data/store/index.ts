08d53f53e3f21e652beab82c0555f2d1d6b54261
Tung Tran <tungtr.dev@gmail.com>
2026-02-08 22:29:27 +0700

    core(redux): store setup

---
+++ src/data/store/index.ts
@@ -0,0 +1,5 @@
+export type { TRootState, TDispatch } from "./store.interface.ts";
+export { store } from "./store.ts";
+
+export { breakpointReducer, breakpointSlice, selectBreakpoint } from "./slices/breakpoint.slice.ts";
+export type { IBreakpointState } from "./slices/breakpoint.slice.interface.ts";
---
+++ src/data/store/slices/breakpoint.slice.ts
@@ -0,0 +1,20 @@
+import { Breakpoint, SliceId } from "@constants";
+import { IBreakpointState } from "./breakpoint.slice.interface.ts";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { TRootState } from "../store.interface.ts";
+
+export const breakpointSlice = createSlice({
+	name: SliceId.Breakpoint,
+	initialState: {
+		value: Breakpoint.Large
+	} as IBreakpointState,
+	reducers: {
+		changeTo: (state, action: PayloadAction<Breakpoint>) => {
+			state.value = action.payload;
+		}
+	}
+});
+
+export const selectBreakpoint = (state: TRootState) => state.breakpoint.value;
+
+export const breakpointReducer = breakpointSlice.reducer;
